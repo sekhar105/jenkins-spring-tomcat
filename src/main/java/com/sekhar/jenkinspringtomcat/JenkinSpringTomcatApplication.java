3a5140fa5001f53982b0b52e9ad6340ee93da879
sekhar105 <repaka99999>
2022-10-27 10:57:54 +0530

    first commit

---
+++ src/main/java/com/sekhar/jenkinspringtomcat/JenkinSpringTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.sekhar.jenkinspringtomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinSpringTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinSpringTomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sekhar/jenkinspringtomcat/JenkinSpringTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekhar.jenkinspringtomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinSpringTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
